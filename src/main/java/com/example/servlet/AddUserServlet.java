c420066517871e8efc46cfed99cd562170f4615b
Makar <hoteng789@gmail.com>
2023-10-31 17:37:42 +0400

    solution

--- src/main/java/com/example/servlet/AddUserServlet.java
+++ src/main/java/com/example/servlet/AddUserServlet.java
@@ -1,5 +1,45 @@
 package com.example.servlet;
 
-public class AddUserServlet {
-    //write your code here!
+import com.example.User;
+import com.example.Warehouse;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/add")
+public class AddUserServlet extends HttpServlet {
+
+
+    @Override
+    protected void doPost(HttpServletRequest request,HttpServletResponse response) throws IOException, ServletException {
+        String firstName = request.getParameter("firstName");
+        String secondName = request.getParameter("lastName");
+
+        User user = new User(firstName,secondName);
+
+        Warehouse.getInstance().addUser(user);
+
+        request.setAttribute("user",user);
+
+        redirectAdd(request,response);
+
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
+        redirectAdd(request,response);
+    }
+
+
+    private void redirectAdd(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/jsp/add.jsp");
+        dispatcher.forward(request,response);
+    }
 }
--- src/main/java/com/example/servlet/GetUsersServlet.java
+++ src/main/java/com/example/servlet/GetUsersServlet.java
@@ -1,5 +1,21 @@
 package com.example.servlet;
 
-public class GetUsersServlet {
-    //write your code here!
+import com.example.Warehouse;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/users")
+public class GetUsersServlet  extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("users", Warehouse.getInstance().getUsers());
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/jsp/users.jsp");
+        dispatcher.forward(request,response);
+    }
+
 }
